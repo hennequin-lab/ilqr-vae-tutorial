1d91de4940b45b1c564010902966768b45d74d01
Marine <marine.schimel@hotmail.fr>
2022-02-24 17:50:36 +0000

    functions

--- tutorial_lib/functions.ml
+++ tutorial_lib/functions.ml
@@ -15,8 +15,8 @@ let plot_us_init all_us i (module P : Plot) =
          let st = Printf.sprintf "l lw 2 lc %i" i in
          item (A (Arr.get_slice [ []; [ i ] ] u)) ~style:st))
     (default_props
-    @ [ xlabel "time /ms"; ylabel ""; title it_title; yrange (-0.03, 0.03) ]
-    @ [ set "object rect from 0,-0.05 to 3,0.05 fc lt 2 fs transparent solid 0.5 front" ]
+    @ [ xlabel "time /ms"; ylabel "inputs"; title it_title; yrange (-0.1, 0.1) ]
+    @ [ set "object rect from 0,-0.1 to 3,0.1 fc lt 2 fs transparent solid 0.5 front" ]
     )
 
 let plot_us_fin all_us i (module P : Plot) =

d2e5ca1a678202cfee521651f5f2f66e6065fd42
hennequin <gje.hennequin@gmail.com>
2022-02-24 15:12:42 +0000

    some tweaking

--- tutorial_lib/functions.ml
+++ tutorial_lib/functions.ml
@@ -3,10 +3,8 @@ open Ilqr_vae
 open Base
 open Gp
 
-  
-  
 (* animation example *)
-let plot_us_init all_us i (module P : Plot) =
+let plot_us ?(range = 0.1) all_us i (module P : Plot) =
   let it_title = Printf.sprintf "Iteration %i" i in
   let u = List.nth_exn all_us i in
   let u = AD.unpack_arr u |> fun z -> Arr.reshape z [| -1; 15 |] in
@@ -15,24 +13,12 @@ let plot_us_init all_us i (module P : Plot) =
          let st = Printf.sprintf "l lw 2 lc %i" i in
          item (A (Arr.get_slice [ []; [ i ] ] u)) ~style:st))
     (default_props
-    @ [ xlabel "time /ms"; ylabel ""; title it_title; yrange (-0.03, 0.03) ]
-    @ [ set "object rect from 0,-0.05 to 3,0.05 fc lt 2 fs transparent solid 0.5 front" ]
-    )
-
-let plot_us_fin all_us i (module P : Plot) =
-  let it_title = Printf.sprintf "Iteration %i" i in
-  let u = List.nth_exn all_us i in
-  let u = AD.unpack_arr u |> fun z -> Arr.reshape z [| -1; 15 |] in
-  P.plots
-    (List.init 10 ~f:(fun i ->
-         let st = Printf.sprintf "l lw 2 lc %i" i in
-         item (A (Arr.get_slice [ []; [ i ] ] u)) ~style:st))
-    (default_props
-    @ [ xlabel "time /ms"; ylabel ""; title it_title; yrange (-0.1, 0.1) ]
-    @ [ set "object rect from 0,-0.1 to 3,0.1 fc lt 2 fs transparent solid 0.5 front" ])
-
+    @ [ xlabel "time /ms"; ylabel ""; title it_title; yrange (-.range, range) ]
+    @ [ set
+          "object rect from 0, graph 0 to first 3, graph 1 fc lt 2 fs transparent solid \
+           0.5 front"
+      ])
 
 let print_msg ?ph s =
   Jupyter_notebook.printf "%s%!" s;
   Jupyter_notebook.display_formatter ?display_id:ph "text/html" |> ignore
-  

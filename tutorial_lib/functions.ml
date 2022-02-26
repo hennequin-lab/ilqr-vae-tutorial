30c510d78a5db7e71e6b1f52476a4e50d1b3ea70
Marine <marine.schimel@hotmail.fr>
2022-02-26 13:18:55 +0000

    updates to figs (minor)

--- tutorial_lib/functions.ml
+++ tutorial_lib/functions.ml
@@ -10,14 +10,15 @@ let plot_us ?(range = 0.1) u i (module P : Plot) =
   P.plots
     (List.init 10 ~f:(fun i ->
          let st = Printf.sprintf "l lw 2 lc %i" i in
-         item (A (Arr.get_slice [ []; [ i ] ] u)) ~style:st))
+         item (A (Arr.get_slice [ []; [ i ] ] u)) ~using:"(($0)*5):1" ~style:st))
     (default_props
-    @ [ xlabel "time [ms]"; ylabel ""; title it_title; yrange (-.range, range) ]
+    @ [ xlabel "time [ms]"; ylabel "inputs"; title it_title; yrange (-.range, range) ]
     @ [ set
-          "object rect from 0, graph 0 to first 6, graph 1 fc lt 2 fs transparent solid \
+          "object rect from 0, graph 0 to first 30, graph 1 fc lt 2 fs transparent solid \
            0.5 front"
       ])
 
 let print_msg ?ph s =
   Jupyter_notebook.printf "%s%!" s;
   Jupyter_notebook.display_formatter ?display_id:ph "text/html" |> ignore
+
